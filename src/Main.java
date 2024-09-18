ebed3075281044c90c9f9bf7345d133fe804b7e6
BT_2S14_14 <laurenw33@nycstudents.net>
2024-09-18 10:02:42 -0400

    Added decimal rounding and better readable to receipt.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
 
         double tipConversion = 0.01;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println("---------------------------------");
+        System.out.println("When inputting data, do NOT use any % or $.");
+        System.out.println("---------------------------------");
 
         Scanner s = new Scanner (System.in);
         System.out.print("Please enter bill amount: ");
@@ -31,17 +37,16 @@ public class Main {
         System.out.println("---------------------------------");
         System.out.println("Here is your bill!");
         System.out.println("---------------------------------");
+        System.out.println("Total Bill (without tip): $" + df.format(billAmount));
+        System.out.println("Tip Percentage: " + df.format(tipPercent) + "%");
+        System.out.println("Total Tip: $" + df.format(totalTip));
         System.out.println();
-        System.out.println("Total Bill (without tip): $" + billAmount);
-        System.out.println("Tip Percentage: " + tipPercent + "%");
-        System.out.println("Total Tip: $" + totalTip);
-        System.out.println();
-        System.out.println("Total Bill: $" + billTotal);
-        System.out.println();
+        System.out.println("Total Bill: $" + df.format(billTotal));
         System.out.println("---------------------------------");
         System.out.println("Costs Per Person:");
-        System.out.println("Total Tip: $" + tipPP);
-        System.out.println("Total Bill: $" + billTotalPP);
+        System.out.println();
+        System.out.println("Total Tip: $" + df.format(tipPP));
+        System.out.println("Total Bill: $" + df.format(billTotalPP ));
         System.out.println("---------------------------------");
 
 
